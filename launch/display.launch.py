e3268c194218ba278117b13e2cedc9df56077b6d
GMEscudeiro <unifgescudeiro@fei.edu.br>
2024-07-01 12:17:25 -0300

    Added laser filter and changed some variable names

--- launch/display.launch.py
+++ launch/display.launch.py
@@ -7,6 +7,7 @@ def generate_launch_description():
     pkg_share = launch_ros.substitutions.FindPackageShare(package='arceus_description').find('arceus_description')
     default_model_path = os.path.join(pkg_share, 'src/urdf/arceus_description.urdf')
     default_rviz_config_path = os.path.join(pkg_share, 'rviz/urdf_config.rviz')
+    navigation_share = launch_ros.substitutions.FindPackageShare(package='arceus_navigation').find('arceus_navigation')
 
     robot_state_publisher_node = launch_ros.actions.Node(
         package='robot_state_publisher',
@@ -34,6 +35,13 @@ def generate_launch_description():
         arguments=['-d', LaunchConfiguration('rvizconfig')],
     )
 
+    laser_filter_params = os.path.join(navigation_share, 'config/laser_filter.yaml')
+    laser_filter = launch_ros.actions.Node(
+        package='laser_filters',
+        executable='scan_to_scan_filter_chain',
+        parameters=[laser_filter_params]
+    )
+
     return launch.LaunchDescription([
         launch.actions.DeclareLaunchArgument(name='gui', default_value='True',
                                             description='Flag to enable joint_state_publisher_gui'),
@@ -46,5 +54,6 @@ def generate_launch_description():
         joint_state_publisher_node,
         joint_state_publisher_gui_node,
         robot_state_publisher_node,
-        rviz_node
+        rviz_node,
+        laser_filter
     ])
